4f3013e2e29dd7e8728de16647e2573d591dca16
Cristian Anderson Quispe Quispe <andersonq378@gmail.com>
2020-08-27 00:50:03 +0000

    Practica final

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,6 +7,21 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  a=13;
+  b=3;
+  c=100;
+  monto = 0;
+  iva = 0;
+  it = 0;
+  utilidad = 0;
   constructor() {}
+
+  calcularOperacion() {
+
+  this.iva = (this.monto * this.a)/this.c;
+  this.it = (this.monto * this.b)/this.c;
+  this.utilidad = this.monto - (this.iva + this.it);
+  
+  }
 
 }
