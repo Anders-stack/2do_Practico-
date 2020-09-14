c1a33d856a163cb718fb1376ef2d13cc6fd3f0b3
Cristian Anderson Quispe Quispe <andersonq378@gmail.com>
2020-09-14 21:52:31 +0000

    Examen Parcial

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,6 +7,99 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  dato = '';
+  v: Array<string> = new Array<string>(5);
+  entrada: number;
+  salida: number;
+  mensaje = ' ';
+
+  constructor() {
+    this.entrada = -1;
+    this.salida = -1;
+  }
+
+  vacia(): boolean {
+    return (this.entrada === -1 && this.salida === -1);
+  }
+
+  llena(): boolean {
+    return ((this.salida === 0 && this.entrada === 4) || (this.entrada === 0 && this.salida === 4));
+  }
+
+  insertarInicio(dato: string) {
+    if (this.vacia()) {
+      this.entrada = this.salida = 0;
+      this.v[this.entrada] = dato;
+      this.dato = ' ';
+    } else {
+      if (!this.llena()) {
+        this.entrada++;
+        this.v[this.entrada] = dato;
+        this.dato = ' ';
+      } else {
+        console.log('Cola llena');
+        this.mensaje = ' Cola Llena';
+      }
+    }
+  }
+  insertarFinal(dato: string) { 
+    if(this.vacia()){
+      this.entrada = this.salida = 4;
+      this.v[this.entrada] = dato;
+      this.dato = ' ';  
+    }
+    else{
+      if(!this.llena()) {
+        this.entrada--;
+        this.v[this.entrada] = dato;
+        this.dato = ' ';
+      }
+      else{
+        console.log('Cola Llena');
+        this.mensaje = ' Cola Llena ';
+      }
+    }
+  }
+
+
+
+   extraerFinal() {
+     if(this.vacia()){
+       this.mensaje = ' Cola Vacia';
+       return;
+     }
+     console.log(this.v[this.salida]);
+     this.mensaje = ' Valor Inicial extraido' + this.v[this.salida];
+     this.v[this.salida] = ' ';
+     if(this.salida === this.entrada) {
+       this.salida =  this.entrada = -1;
+     }
+     else{
+       this.salida--;
+     }
+    
+  }
+
+  extraerInicio() {
+    if (this.vacia()){
+      this.mensaje = 'Cola vacia';
+      return;
+    }
+    console.log(this.v[this.salida]);
+    this.mensaje = 'Valor Final extraido' + this.v[this.salida];
+    this.v[this.salida] = ' ';
+    if(this.entrada === this.salida) {
+      this.entrada = this.salida = -1;
+    }
+    else{
+      this.salida++;
+    }
+  }
+ 
+ 
+
+  ver() {
+    console.log(this.v);
+  }
 
 }
